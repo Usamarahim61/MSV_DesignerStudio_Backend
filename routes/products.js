c5bc675139df562d43bca7ee5649c41d62b047f6
Usamarahim61 <usamarahim61@gmail.com>
2026-02-06 17:41:43 +0500

    increase the image size validation limit

--- routes/products.js
+++ routes/products.js
@@ -15,7 +15,7 @@ const router = express.Router();
 // Configure multer for Cloudinary image uploads
 const upload = multer({
   storage,
-  limits: { fileSize: 10 * 1024 * 1024 }, // 10MB
+  limits: { fileSize: 50 * 1024 * 1024 }, // 50MB
 });
 
 // @route   GET /api/products
